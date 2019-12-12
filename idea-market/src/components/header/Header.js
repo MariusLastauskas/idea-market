54d79adc0ae8f1a1baec238aa45a3d683c0b40dd
MariusLastauskas <marius20122@gmail.com>
2019-12-12 16:20:30 +0200

    FE implementation

---
+++ idea-market/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../logo/Logo';
+
+const Header = () => {
+    return (
+        <div>
+            <Logo />
+        </div>
+    );
+};
+
+export default Header;
---
+++ idea-market/src/components/logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <span>Idea market</span>
+    );
+};
+
+export default Logo;
